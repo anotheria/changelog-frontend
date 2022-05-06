555af4560829e81ad8a38664c2420e6028371c20
saenq <vsaenko4@gmail.com>
2022-05-06 03:27:19 +0300

    add form validation

--- src/components/ChangelogModalForm.vue
+++ src/components/ChangelogModalForm.vue
@@ -3,11 +3,11 @@
     <div class="modal">
       <div class="modal__backdrop" @click="$emit('closeModal', false)"></div>
       <div class="modal__content">
-        <form @submit.prevent="saveItem()" class="modal__form">
+        <form @submit.prevent="saveItem" class="modal__form">
           <button
-            type="button"
-            class="modal__btn-close"
-            @click="$emit('closeModal', false)"
+              type="button"
+              class="modal__btn-close"
+              @click="$emit('closeModal', false)"
           >
             <img src="@/style/icon/close.svg" alt=""/>
           </button>
@@ -15,9 +15,10 @@
             <span class="modal__label-text"> Reason </span>
             <div class="modal__input-wrap">
               <input
-                v-model="formData.reason"
-                class="modal__input"
-                type="text"
+                  v-model.trim="formData.reason"
+                  :class="{'modal__input--invalid': v$.formData.reason.$error}"
+                  class="modal__input"
+                  type="text"
               />
             </div>
           </div>
@@ -25,9 +26,9 @@
             <span class="modal__label-text"> Message </span>
             <div class="modal__input-wrap">
               <input
-                v-model="formData.message"
-                class="modal__input"
-                type="text"
+                  v-model.trim="formData.message"
+                  class="modal__input"
+                  type="text"
               />
             </div>
           </div>
@@ -35,9 +36,10 @@
             <span class="modal__label-text"> Author </span>
             <div class="modal__input-wrap">
               <input
-                v-model="formData.author"
-                class="modal__input"
-                type="text"
+                  v-model.trim="formData.author"
+                  :class="{'modal__input--invalid': v$.formData.author.$error }"
+                  class="modal__input"
+                  type="text"
               />
             </div>
           </div>
@@ -45,8 +47,9 @@
           <div class="modal__label">
             <span class="modal__label-text"> Date </span>
             <Datepicker
-              v-model="currentDate"
-              :format="'dd/MM/yyyy HH:mm'"
+                v-model="currentDate"
+                :class="{'dp--invalid': v$.currentDate.$error}"
+                :format="'dd/MM/yyyy HH:mm'"
             ></Datepicker>
           </div>
 
@@ -54,21 +57,31 @@
             <span class="modal__label-text"> Type </span>
             <div class="modal__input-wrap">
               <v-select
-                v-model="formData.type"
-                :options="filtersList.type"
-              ></v-select>
+                  v-model="formData.type"
+                  :class="{'vs--invalid': v$.formData.type.$error }"
+                  :options="filtersList.type"
+              >
+                
+                <template #open-indicator="{ attributes }">
+                  <span v-bind="attributes"><img src="@/style/icon/down.svg" alt=""></span>
+                </template>
+              </v-select>
             </div>
           </div>
           <div class="modal__label">
             <span class="modal__label-text"> Tags </span>
             <div class="modal__input-wrap">
               <v-select
-                ref="select"
-                v-model="formData.tags"
-                :options="filtersList.tags"
-                taggable
-                multiple
-              ></v-select>
+                  ref="select"
+                  v-model="formData.tags"
+                  :options="filtersList.tags"
+                  taggable
+                  multiple
+              >
+                <template #open-indicator="{ attributes }">
+                  <span v-bind="attributes"><img src="@/style/icon/down.svg" alt=""></span>
+                </template>
+              </v-select>
             </div>
           </div>
 
@@ -82,52 +95,89 @@
 </template>
 
 <script lang="js">
-  import axios from "axios";
+import axios from "axios";
+import Datepicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+import vSelect from 'vue-select'
+import "vue-select/dist/vue-select.css";
+import useVuelidate from '@vuelidate/core'
+import {required} from '@vuelidate/validators'
 
-  import Datepicker from "@vuepic/vue-datepicker";
-  import "@vuepic/vue-datepicker/dist/main.css";
-
-  import vSelect from 'vue-select'
-  import "vue-select/dist/vue-select.css";
-
-  export default {
-    components: {
-      Datepicker,
-      vSelect
+export default {
+  components: {
+    Datepicker,
+    vSelect,
+  },
+  props: {
+    filtersList: {
+      type: Object,
+      required: true
     },
-    props: {
-      filtersList: {
-        type: Object,
-        required: true
-      },
-      currentFormData: {
-        type: Object
-      }
-    },
-    data: () => ({
+    currentFormData: {
+      type: Object
+    }
+  },
+  setup() {
+    console.log('setup');
+    return {
+      v$: useVuelidate()
+    }
+  },
+  data() {
+    return {
       currentItemId: null,
-      formData: {},
-      currentDate: new Date()
-    }),
+      formData: {
+        id: null,
+        timestamp: new Date(),
+        author: "",
+        message: "",
+        reason: "",
+        tags: [],
+        type: null,
+      },
+      currentDate: new Date(),
+      attributes: {
+        'ref': 'openIndicator',
+        'role': 'presentation',
+        'class': 'vs__open-indicator',
+      }
+    }
+  },
 
-    methods: {
-      saveItem() {
-        this.formData.timestamp = this.currentDate.getTime();
-        axios
+  validations() {
+    return {
+      formData: {
+        reason: {required},
+        author: {required},
+        type: {required},
+      },
+      currentDate: {required}
+    }
+  },
+
+  methods: {
+    saveItem() {
+      if (this.v$.$invalid) {
+        this.v$.$touch();
+        return
+      }
+
+      this.formData.timestamp = this.currentDate.getTime();
+      axios
           .post("api/changelog/save/", this.formData)
           .then(() => {
             this.$emit("updateList");
             this.$emit("closeModal", false);
           })
           .catch((error) => console.log(error));
-      },
-    },
-
-    mounted() {
-      this.formData = this.currentFormData;
-      this.currentDate = new Date(this.currentFormData.timestamp);
     }
-  };
+  },
+
+  mounted() {
+    this.formData = this.currentFormData;
+    this.currentDate = new Date(this.currentFormData.timestamp);
+  }
+};
 </script>
 
 <style scoped lang="scss"></style>
